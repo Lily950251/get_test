1be2605de2cb0c1a1ec363b88e60a9fa2633de0c
user <user@gmail.com>
2025-05-23 10:49:04 +0800

    新增讓使用者輸入數字的功能 替換數字

--- text.py
+++ text.py
@@ -1,6 +1,6 @@
 num = int(input("請輸入一個數字："))
-for i in range(1,10):
-    for j in range(1,10):
+for i in range(1,num+1):
+    for j in range(1,num+1):
         print("%d*%d = %d"%(i,j,i*j), end = " ")
     print()
         
